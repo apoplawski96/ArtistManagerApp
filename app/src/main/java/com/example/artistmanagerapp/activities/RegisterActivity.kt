9e54ac5c4c83c717887c8a073991b7bdc641c8a4
arturpoplawski <a.poplawski96@gmail.com>
2019-02-04 20:46:01 +0100

    Small progress in register functionality.

--- app/src/main/java/com/example/artistmanagerapp/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/artistmanagerapp/activities/RegisterActivity.kt
@@ -3,6 +3,8 @@ package com.example.artistmanagerapp.activities
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import com.example.artistmanagerapp.R
 import com.google.firebase.FirebaseApp
@@ -11,13 +13,31 @@ import com.google.firebase.auth.FirebaseUser
 
 class RegisterActivity : AppCompatActivity() {
 
+    // Firebase stuff
     private lateinit var auth: FirebaseAuth
+
+    // View components
+    private lateinit var registerEmailEditText : EditText
+    private lateinit var registerPasswordEditText : EditText
+    private lateinit var registerPasswordRepeatEditText : EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        // Firebase stuff
         FirebaseApp.initializeApp(this)
+        auth = FirebaseAuth.getInstance()
+
+        val registerButton : Button = findViewById(R.id.register_button) as Button
+        registerEmailEditText = findViewById(R.id.register_email_edit_text) as EditText
+        registerPasswordEditText = findViewById(R.id.register_password_edit_text) as EditText
+        registerPasswordRepeatEditText = findViewById(R.id.register_passwordrepeat_edittext) as EditText
+
+        registerButton.setOnClickListener {
+
+        }
+
     }
 
     fun signUp(email : String, password : String){
