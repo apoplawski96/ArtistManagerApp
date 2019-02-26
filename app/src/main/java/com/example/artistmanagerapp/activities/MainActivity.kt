8932abbb3ec39b87598df1a446e1473ffe4f8ea9
arturpoplawski <a.poplawski96@gmail.com>
2019-02-26 14:36:35 +0100

    Basic BotomNavBar prototype implemented.

--- app/src/main/java/com/example/artistmanagerapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/artistmanagerapp/activities/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.artistmanagerapp.activities
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.design.widget.NavigationView
 import android.widget.TextView
 import com.example.artistmanagerapp.R
 import com.example.artistmanagerapp.models.Artist
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +20,9 @@ class MainActivity : AppCompatActivity() {
 
         val auth = FirebaseAuth.getInstance()
 
+        bottom_nav_bar.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        //to delete later
         val logout = findViewById(R.id.logout_text_view) as TextView
         logout.setOnClickListener {
             auth.signOut()
@@ -25,7 +31,22 @@ class MainActivity : AppCompatActivity() {
             }
             startActivity(intent)
         }
-
-
     }
+
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {item ->
+        when (item.itemId){
+            R.id.home -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.frag2 -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.frag3 -> {
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
 }
